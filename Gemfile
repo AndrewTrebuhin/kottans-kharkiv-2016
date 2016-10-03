26f8c77d9db11eebed30145539f7a1f523caaad7
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2016-10-03 14:15:12 +0300

    Add new & show pages, add pg to gemfile.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,9 @@ source "https://rubygems.org"
 ruby '2.3.0'
 
 gem 'sinatra', '1.4.7'
+gem 'data_mapper'
+gem 'pg'
+
+group :development do
+  gem 'dm-sqlite-adapter'
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,38 @@
-require "sinatra"
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite:./db/base.db')
+
+class Message
+  include DataMapper::Resource
+  property :id, Serial
+  property :body, Text
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do
-  "Hi there!"
+  erb :new
+end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'index'
+end
+
+get '/messages/new' do
+  erb :'new'
+end
+
+post '/messages/new' do
+  params.delete 'submit'
+  @message = Message.create(params)
+  erb :'show_link'
+end
+
+get '/message/:id' do
+  @message = Message.get(params[:id])
+  erb :'show'
 end
