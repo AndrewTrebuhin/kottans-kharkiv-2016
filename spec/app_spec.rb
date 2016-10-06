04ed8e75aab8c37663109d4ad6f1467675203fb4
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2016-10-06 16:30:32 +0300

    Tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,36 @@
 require File.expand_path '../spec_helper.rb', __FILE__
+
 describe "My Sinatra Application" do
-  it "should allow accessing the home page" do
+  it 'should show home page' do
     get '/'
     expect(last_response).to be_ok
   end
+
+  it 'should show messages page' do
+    get '/messages'
+
+    expect(last_response.body).to include('<h1>All messages</h1>')
+  end
+
+  it 'should show new message page' do
+    get '/messages/new'
+
+    expect(last_response.body).to include('<h2>New message</h2>')
+  end
+
+  it 'should create a message' do
+    Message.create(url: 'randomurl', delete_after: 3, body: 'Some text')
+
+    get '/message/randomurl'
+
+    expect(last_response.body).to include('<h2>randomurl</h2>')
+    expect(last_response.body).to include('Some text')
+  end
+
+  it 'should show an error' do
+    get '/message/wrongurl'
+
+    expect(last_response.body).to include('Message isn\'t found!')
+  end
+
 end
